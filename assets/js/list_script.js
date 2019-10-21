70c688a8a08cb152d36c474c3cc2b896d4e0e248
Sam Hall <sammchall@gmail.com>
2019-10-21 16:00:09 +1100

    updated pages

---
+++ assets/js/list_script.js
@@ -0,0 +1,21 @@
+/* Inspired by Pieter Biesemans Code snippet Repository https://codepen.io/pieter-biesemans/pen/zjMOex */
+
+hljs.initHighlightingOnLoad();
+
+var listOptions = {
+  valueNames: ['name', 'lang', 'code']
+};
+
+var snippetList = new List('repo', listOptions);
+
+ snippetList.on('updated', function (list) {
+   var items = document.querySelectorAll('.snippet');
+   items.forEach(function(item){
+     item.style.opacity = 0;
+   });
+   setTimeout(function(){
+     items.forEach(function(item){
+       item.style.opacity = 1;
+     });
+   },150);
+ });
